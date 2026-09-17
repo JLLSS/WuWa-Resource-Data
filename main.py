c82c607a08f902fcaf6779bdd566c954327d7c5e
moonswave <83159590+555me@users.noreply.github.com>
2026-09-17 15:34:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,22 +5,22 @@ import json
 launcher = {
     "cn": {
         "beta": "https://prod-cn-alicdn-gamestarter.kurogame.com/launcher/launcher/10008_Pa0Q0EMFxukjEqX33pF9Uyvdc8MaGPSz/G152/index.json",
-        "live": "https://prod-cn-alicdn-gamestarter.kurogame.com/launcher/launcher/10003_Y8xXrXk65DqFHEDgApn3cpK5lfczpFx5/G152/index.json"
+        "live": "https://prod-cn-alicdn-gamestarter.kurogame.com/launcher/app/10003_oLNgHF1CESo51DGHN2odtp40e3oI1HfZ/index.json"
     },
     "os": {
         "beta": "https://prod-volcdn-gamestarter.kurogame.net/launcher/launcher/50013_HiDX7UaJOXpKl3pigJwVxhg5z1wllus5/G153/index.json",
-        "live": "https://prod-volcdn-gamestarter.kurogame.net/launcher/launcher/50004_obOHXFrFanqsaIEOmuKroCcbZkQRBC7c/G153/index.json"
+        "live": "https://prod-alicdn-gamestarter.kurogame.com/launcher/app/50004_P7xcUZnEr1AXIGON25E6KjpOgTlVrg6e/index.json"
     }
 }
 
 game = {
     "cn": {
         "beta": "https://prod-cn-alicdn-gamestarter.kurogame.com/launcher/game/G152/10008_Pa0Q0EMFxukjEqX33pF9Uyvdc8MaGPSz/index.json",
-        "live": "https://prod-cn-alicdn-gamestarter.kurogame.com/launcher/game/G152/10003_Y8xXrXk65DqFHEDgApn3cpK5lfczpFx5/index.json"
+        "live": "https://prod-cn-alicdn-gamestarter.kurogame.com/launcher/game/10003_oLNgHF1CESo51DGHN2odtp40e3oI1HfZ/G152/official/index.json"
     },
     "os": {
         "beta": "https://prod-alicdn-gamestarter.kurogame.com/launcher/game/G153/50013_HiDX7UaJOXpKl3pigJwVxhg5z1wllus5/index.json",
-        "live": "https://prod-alicdn-gamestarter.kurogame.com/launcher/game/G153/50004_obOHXFrFanqsaIEOmuKroCcbZkQRBC7c/index.json"
+        "live": "https://prod-alicdn-gamestarter.kurogame.com/launcher/game/50004_P7xcUZnEr1AXIGON25E6KjpOgTlVrg6e/G153/official/index.json"
     }
 }
 
@@ -32,7 +32,10 @@ def get_launcher_version(url):
 def get_game_version(url):
     response = requests.get(url)
     data = response.json()
-    return data.get("default", {}).get("version", None), data
+    if not data.get("resourcePacks",{}):
+        return data.get("default",{}).get("version",None),data
+    else:
+        return data.get("resourcePacks", {}).get("common", {}).get("version",None), data
 
 def save_json_to_file(directory, filename, data):
     if not os.path.exists(directory):
